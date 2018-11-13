87466ac2abd1d525320ffc93821edeba7e4564cb
al1100ol-s <al1100ol-s@falk-8.e.lth.lu.se>
2018-11-13 15:09:03 +0100

    Intiering. Skapat privata funktioner, samt påbörjat solve-funktionen.

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -1,16 +1,53 @@
 
 public class Sudoku {
 	private int[][] grid;
+	private int index;
+	
+	public static void main (String[] args) {
+		Sudoku s = new Sudoku();
+		System.out.println(s.getField(0, 0));
+	}
 	
 	public Sudoku () {
 		grid = new int[9][9];
+		index = 0;
 	}
 	
-	public void setField (int x, int y, int value) {
-		grid[x][y] = value;
+	public boolean setField (int r, int c, int value) {
+		if (getField(r, c) != 0) {
+			return false;
+		} else {
+			grid[r][c] = value;
+			return true;
+		}
 	}
 	
-	public int getField (int x, int y) {
-		return grid[x][y];
+	public int getField (int r, int c) {
+		return grid[r][c];
 	}
+	
+	public void clear () {
+		grid = new int[9][9];
+	}
+	
+	public boolean solve () {
+		return solve(0, 0);
+	}
+	
+	private boolean solve (int r, int c) {
+		if (r == 9) {		// This is outside the board, meaning all previous ones have worked
+			return true;
+		} else {
+			for (int col = 0; col < 9; col++) {	// Loop through the columns
+				int num = 1;	// Counter 1-9
+				if (getField(r, col) == 0) {	// Checks if the current field is empty
+					
+				}
+			}
+		}
+		
+		return false;
+	}
+	
+	
 }
