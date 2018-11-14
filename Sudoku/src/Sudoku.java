97715371c10f5c67962b39e7031d702265e49ba3
al1100ol-s <al1100ol-s@eka-5.net.lth.se>
2018-11-14 14:07:39 +0100

    Grundreglerna är klara Jobba vidare på rekursion och javaFX

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -13,6 +13,14 @@ public class Sudoku {
 		index = 0;
 	}
 	
+	/**
+	 * Returns true if a value is successfully added to a field on r, c.
+	 * 
+	 * @param r the row to alter
+	 * @param c the column to alter
+	 * @param value the value to assign the chosen field
+	 * @return true if a value was successfully added, false otherwise
+	 */
 	public boolean setField (int r, int c, int value) {
 		if (getField(r, c) != 0) {
 			return false;
@@ -35,19 +43,53 @@ public class Sudoku {
 	}
 	
 	private boolean solve (int r, int c) {
-		if (r == 9) {		// This is outside the board, meaning all previous ones have worked
-			return true;
-		} else {
-			for (int col = 0; col < 9; col++) {	// Loop through the columns
-				int num = 1;	// Counter 1-9
-				if (getField(r, col) == 0) {	// Checks if the current field is empty
-					
+		return false;
+	}
+	
+	// Checks all three rules
+	private boolean ruleCheck (int row, int col, int value) {
+		return (checkRow(row, col, value) && checkColumn(row, col, value) && checkArea(row, col, value));
+	}
+	
+	// This will return true if the specified value fits to the row.
+	private boolean checkRow (int row, int col, int value) {
+		for (int i = 0; i < 9; i++) {						// Loops through the row
+			if (col != i && getField(row, i) == value) {	// Doesn't check itself
+				return false;								// Checks if the value is in the row
+			}
+		}
+		
+		grid[row][col] = value;		// Otherwise, assign the value and return true
+		return true;
+	}
+	
+	// This will return true if the specified value fits to the column
+	private boolean checkColumn (int row, int col, int value) {
+		for (int i = 0; i < 9; i++) {						// Loops through the row
+			if (row != i && getField(i, col) == value) {	// Doesn't check itself
+				return false;								// Checks if the value is in the row
+			}
+		}
+		
+		grid[row][col] = value;		// Otherwise, assign the value and return true
+		return true;
+	}
+	
+	// This will return true if the specified value fits to the current area
+	private boolean checkArea (int row, int col, int value) {
+		int rowMin = row - (row % 3);	// 0 <= (row % 3) <= 2 
+		int rowMax = rowMin + 3;
+		int colMin = col - (col % 3);	// 0 <= (col % 3) <= 2
+		int colMax = colMin + 3;
+		
+		for (int i = rowMin; i < rowMax; i++) {						// Loops within row-area
+			for (int j = colMin; i < colMax; i++) {					// Loops within column-area
+				if (row != i && col != j && grid[i][j] == value) {	// Checks for values and coordinates
+					return false;
 				}
 			}
 		}
 		
-		return false;
+		return true;
 	}
-	
-	
 }
