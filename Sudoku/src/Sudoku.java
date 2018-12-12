da1f5f8b5d7edbf706ca4b8f75813bde565413c0
Administratör <Administratr@HPProBook>
2018-12-12 20:31:22 +0100

    tests

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -74,7 +74,7 @@ public class Sudoku {
 		System.out.println("\n\n______________________________________\n\n");
 	}
 
-	private boolean solve() {
+	public boolean solve() {
 		for(int row = 0; row < 9; row++) {
 			for(int col = 0; col < 9; col++) {
 				if(getField(row, col) == 0) {
--- Sudoku/src/SukdokuTest.java
+++ Sudoku/src/SukdokuTest.java
@@ -7,69 +7,79 @@ import org.junit.Before;
 public class SukdokuTest {
 
 	private Sudoku sudoku;
-	
+
 	@Before
-	public void setUp () {
+	public void setUp() {
 		sudoku = new Sudoku();
 	}
-	
+
 	@After
-	public void tearDown () {
-		
+	public void tearDown() {
+		sudoku.clear();
 	}
-	
+
 	@Test
-	public void testSolveEmpty () {
+	public void testSolveEmpty() {
 		assertTrue("Can't solve empty sudoku", sudoku.solve());
-	}
-	
-	@Test
-	public void testUnsolvable () {
-		
-	}
-	
-	@Test
-	public void testSolve () {
-		
-	}
-	
-	@Test
-	public void testClear () {
-		boolean equals = true;
-		Sudoku s = new Sudoku();
-		s.setField(0, 0, 5);
-		s.clear();
 		
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				if (sudoku.getField(i, j) != 0 || s.getField(i, j) != 0) {
-					equals = false;
-				}
+				assertTrue("Board isn't filled", sudoku.getField(i, j) != 0);
 			}
 		}
+	}
+
+	@Test
+	public void testUnsolvable() {
+		sudoku.setField(0, 0, 1);
+		sudoku.setField(0, 1, 1);
+		assertFalse("Impossible sudoku was 'solved'", sudoku.solve());
+	}
+
+	@Test
+	public void testSolve() {
+
+	}
+
+	@Test
+	public void testClear() {
 		
-		assertTrue ("Doesn't clear sudoku properly", equals);
+		sudoku.setField(0, 0, 1);
+		sudoku.setField(0, 8, 9);
+		sudoku.setField(4, 5, 3);
+		sudoku.setField(2, 7, 8);
+		sudoku.setField(1, 8, 2);
+		
+		sudoku.clear();
+		
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				assertTrue("Doesn't clear properly", sudoku.getField(i, j) == 0);
+			}
+		}
 	}
-	
+
 	@Test
-	public void testField () {
-		//assertTrue("Can't enter value to field", sudoku.setField(0, 0, 5));
-		assertTrue("Can't get value from field", sudoku.getField(0, 0) == 5);
+	public void testField() {
+		assertTrue("Can't get field", sudoku.getField(0, 0) == 0);
+		sudoku.setField(0, 0, 1);
+		assertTrue("Can't set field properly", sudoku.getField(0, 0) == 1);
 	}
-	
+
 	@Test
-	public void testRulesRow () {
+	public void testRulesRow() {
 		sudoku.setField(0, 0, 1);
 		assertFalse("Row rulecheck failed", sudoku.ruleCheck(0, 7, 1));
 	}
-	
+
 	@Test
-	public void testRulesCol () {
+	public void testRulesCol() {
 		sudoku.setField(0, 0, 1);
 		assertFalse("Column rulecheck failed", sudoku.ruleCheck(7, 0, 1));
 	}
-	
-	@Test public void testRulesArea () {
+
+	@Test
+	public void testRulesArea() {
 		sudoku.setField(0, 0, 1);
 		assertFalse("Area rulecheck failed", sudoku.ruleCheck(2, 2, 1));
 	}
