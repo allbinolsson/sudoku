ed7c46a87cedca65359d2e24375d5403cbf1dd1e
Mathias <Mathias@LAPTOP-N7BMEOOH>
2018-12-13 11:21:17 +0100

    COmments in viewer

--- Sudoku/src/SudokuViewer.java
+++ Sudoku/src/SudokuViewer.java
@@ -1,17 +1,12 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -22,7 +17,14 @@ public class SudokuViewer extends Application {
 	private TextField[][] fields = new TextField[9][9];
 	private Sudoku sudoku = new Sudoku();
 	
-
+	
+	/**
+	 * Get the number entered into a textfield
+	 * 
+	 * @param x the row in array
+	 * @param y the column in array
+	 * @return value of specified field or -1 if it is empty
+	 */
 	private int getField(int x, int y) {
 		if(!fields[x][y].getText().isEmpty()) {
 			return Integer.parseInt(fields[x][y].getText());
@@ -31,6 +33,13 @@ public class SudokuViewer extends Application {
 		}
 	}
 	
+	/**
+	 * Sets the text in a textfield
+	 * 
+	 * @param x the row in array
+	 * @param y the column in array
+	 * @param value which wants to be set
+	 */
 	private void setField(int x, int y, int value) {
 		if(x < 9 && y < 9 && value != 0) {
 			fields[x][y].setText(Integer.toString(value));			
@@ -39,6 +48,9 @@ public class SudokuViewer extends Application {
 		}
 	}
 	
+	/**
+	 * Send an update to model
+	 */
 	private void updateSudoku() {
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
@@ -49,6 +61,9 @@ public class SudokuViewer extends Application {
 		}
 	}
 	
+	/**
+	 * Updates array from the model
+	 */
 	private void updateBoard() {
 		for(int i = 0; i < 9; i++) {
 			for(int j = 0; j < 9; j++) {
@@ -57,14 +72,17 @@ public class SudokuViewer extends Application {
 		}
 	}
 
+	
+	/**
+	 * JavaFX start
+	 */
 	@Override
 	public void start(Stage stage) throws Exception {
-		HBox root = new HBox(); // eller annan komponent
-		TilePane board = new TilePane();
+		HBox root = new HBox(); // Root contains everything in the window
+		TilePane board = new TilePane(); //Board Contains the Sudoku board
+			
 		
-		
-		
-		// Skapa ytterligare komponenter och lägg till dem i root
+		// Modify the visuals of the window
 		board.setMinSize(boardSize, boardSize);
 		board.setMaxSize(boardSize, boardSize);
 		board.setStyle("-fx-background-color: #000000;");
@@ -74,7 +92,7 @@ public class SudokuViewer extends Application {
 		root.setMinHeight(boardSize);
 		root.setStyle("-fx-background-color: #00F0F0;");
 		
-		int group = 0;
+		// Used for creating the areas and textfields within
 		TilePane temp;
 		OneLetterTextField tf;
 		int r;
@@ -82,23 +100,25 @@ public class SudokuViewer extends Application {
 		// Create the 9 areas
 		for(int i = 0; i < 9; i++) {
 			temp = new TilePane();
+			
+			// Size of areas
 			temp.setMinSize(boardSize / 3, boardSize / 3);
 			temp.setMaxSize(boardSize / 3, boardSize / 3);
 
 			temp.setPrefColumns(3);
 			
 			// Calculate startingposition of first square in area
-			if(group % 3 == 0) {
+			if(i % 3 == 0) {
 				c = 0;
-			} else if(group % 3 == 1) {
+			} else if(i % 3 == 1) {
 				c = 3;
 			} else {
 				c = 6;
 			}
 			
-			if(group < 3) {
+			if(i < 3) {
 				r = 0;
-			} else if(group >= 3 && group < 6) {
+			} else if(i >= 3 && i < 6) {
 				r = 3;
 			} else {
 				r = 6;
@@ -111,30 +131,31 @@ public class SudokuViewer extends Application {
 				tf.setFont(Font.font("Verdana", FontWeight.BOLD, 25));
 				tf.setPrefWidth(boardSize / 9);
 				tf.setMinHeight(boardSize / 9);
-				if(group % 2 == 0) {
+				if(i % 2 == 0) {
 					tf.setStyle("-fx-background-color: #b7b7b7;");
-				} else if (group % 2 == 1) {
+				} else if (i % 2 == 1) {
 					tf.setStyle("-fx-background-color: #FFFFFF;");
 				}
 				
 
-				
+				// Add the textfield to aray
 				fields[r][c] = tf;
 				c++;
 				if(c % 3 == 0) {
 					c = oldC;
 					r++;
 				}
-				
+				// Add textfield to area
 				temp.getChildren().add(tf);
 			}
-			group++;
+			// Add area to board
 			board.getChildren().add(temp);
 		}
 		
 		Button bSolve = new Button("Solve");
 		Button bClear = new Button("Clear");
 		
+		// Listner for solve button
 		bSolve.setOnAction((event) -> {
 			updateSudoku();
 			if(sudoku.solveSudoku()) {
@@ -147,11 +168,13 @@ public class SudokuViewer extends Application {
 			}
 		});
 		
+		// Listener for clear button
 		bClear.setOnAction((event) -> {
 			sudoku.clear();
 			updateBoard();
 		});
 		
+		// Add everything to window and show window
 		root.getChildren().addAll(board, bSolve, bClear);
 		Scene scene = new Scene(root, 1280, 720);
 		stage.setScene(scene);
@@ -163,6 +186,11 @@ public class SudokuViewer extends Application {
 	}
 	
 
+	/**
+	 * Main function
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
